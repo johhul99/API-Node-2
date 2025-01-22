70a8d97fa07d6f1711f46d3f720c1d7ef0e63d11
Johan Hultin <johan.hultin99@gmail.com>
2025-01-22 21:17:17 +0100

    post, get

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+let messages = [];
+let currentId = 1;
+
+
+app.get('/api/messages', (req, res)=>{
+    console.log('Getting all messages.');
+    res.json(messages);
+});
+
+app.post('/api/messages', (req, res)=>{
+    const { text } = req.body;
+
+    if(!text){
+        return res.status(400);
+    }
+
+    const newMessage = {
+        id: currentId++,
+        text,
+        createdAt: new Date()
+    };
+
+    messages.push(newMessage);
+    res.status(200).json(newMessage);
+});
+
 
 app.listen(PORT, ()=>{
     console.log('Server is up and running on port 3000!');
